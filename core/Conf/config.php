e4f8b531ddc35810034d54f052a4a55eb8783c9c
legend <zcq.0@163.com>
2013-06-03 00:02:42 +0800

    新增模版，优化首页首屏。

--- core/Conf/config.php
+++ core/Conf/config.php
@@ -6,7 +6,7 @@ $config = require './config.php';
 $array = array(
 	'APP_GROUP_LIST'      =>'Admin,Home,Plus',
 	'TMPL_FILE_DEPR'      =>'_',//模板文件MODULE_NAME与ACTION_NAME之间的分割符，只对项目分组部署有效
-	'USER_AUTH_KEY'       =>'gxcms',// 用户认证SESSION标记
+	'USER_AUTH_KEY'       =>'mxdyy',// 用户认证SESSION标记
 	'NOT_AUTH_ACTION'     =>'index,show,add,top,left,main',// 默认无需认证操作
 	'REQUIRE_AUTH_MODULE' =>'Config,Cache,Master,Channel,Video,Info,Special,Collect,User,Userview,Comment,Gbook,Tpl,Adsense,Link,Upload,Html,Data,Slide',// 默认需要认证模型
 	'URL_CASE_INSENSITIVE'=>true,//URL是否不区分大小写 默认区分大小写
@@ -22,11 +22,11 @@ $array = array(
     //'SHOW_ADV_TIME'	  => true,   // 显示详细的运行时间
     //'SHOW_DB_TIMES'	  => true,   // 显示数据库查询和写入次数
 	'cms_admin'           =>'index.php',
-	'cms_name'            =>'光线影视内容管理系统',
+	'cms_name'            =>'梦想电影院管理系统',
 	'cms_var'             =>'1.5',
-	'cms_url'             =>'http://www.gxcms.com/',
+	'cms_url'             =>'http://www.mxdyy.com.com/',
 	'cms_urlvar'          =>'http://union.gxcms.com/app/version.js',
-	'cms_exts'            =>'jpg,jpeg,gif,png',	
+	'cms_exts'            =>'jpg,jpeg,gif,png',
 	'cms_col_content'     =>'[内容]',
 );
 return array_merge($config,$array);
--- core/Conf/define.php
+++ core/Conf/define.php
@@ -1,6 +1,6 @@
 <?php
 define('THINK_PATH', './core/ThinkPHP');
-define('RUNTIME_PATH','./temp/');
+define('RUNTIME_PATH', './temp/');
 define('APP_PATH', './core');
 define('APP_NAME', 'gxcms');
 ?>
